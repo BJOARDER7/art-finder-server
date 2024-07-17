1fadb8ff122be6080b7a73d61ffc74868a665066
bjoarder7 <bjoarder87@gmail.com>
2024-07-18 01:07:49 +0600

    view details page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.get('/craft/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+     })
 
     app.post('/craft', async(req, res) => {
       const newCraft = req.body;
